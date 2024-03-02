98c0ae3ff3cfab76a8b098622680f97751fb773c
Christian Dalager <christian.dalager@eksponent.com>
2024-03-02 15:05:44 +0100

    Get CosmosDbConnstr from Keyvault

--- iac/create_resources.ps1
+++ iac/create_resources.ps1
@@ -11,6 +11,8 @@ $database = "commitstore-db"
 $partitionKey = "/repository"
 $container = "commits"
 
+$keyvault = "commitstore-kv"
+$subscriptionId = az account show --query id -o tsv
 #
 # Create resource group
 az group create --name $resourcegroup --location $location
@@ -34,34 +36,60 @@ az functionapp config set -g $resourcegroup -n $functionappname --use-32bit-work
 az functionapp identity assign -g $resourcegroup -n $functionappname
 
 # get the identity
-$identity = az functionapp identity show -g $resourcegroup -n $functionappname --query "principalId" -o tsv
+$funcidentity = az functionapp identity show -g $resourcegroup -n $functionappname --query "principalId" -o tsv
 
 
+az keyvault create --name $keyvault --resource-group $resourcegroup --enable-rbac-authorization true
 
-# ------------------ THIS IS A TEST COMMITLOGGER QUEUE ----------------
-# create queue
-az storage queue create --name $storagequeue --account-name $storageaccount
+# grant rbac read access to the function app
 
-# create shared access policy for pushing messages
-az storage queue policy create --account-name $storageaccount --queue-name $storagequeue --name "addcommits" --permissions "a" --expiry "2028-12-31T23:59:00Z"
+az role assignment create `
+    --assignee "$funcidentity" `
+    --role "Key Vault Reader" `
+    --scope "/subscriptions/$subscriptionid/resourcegroups/$resourcegroup/providers/Microsoft.KeyVault/vaults/$keyvault"
 
-# create a sas token for pushing messages
-$token = az storage queue generate-sas --account-name $storageaccount --name $storagequeue --policy-name "addcommits" -o tsv 
+az role assignment create `
+    --assignee "$funcidentity" `
+    --role "Key Vault Secrets User" `
+    --scope "/subscriptions/$subscriptionid/resourcegroups/$resourcegroup/providers/Microsoft.KeyVault/vaults/$keyvault"
 
-Write-Output "SAS token for pushing messages:`n $token"
+# Add cosmosdb connection string to key vault
 
-$storageendpoint = az storage account show --name $storageaccount --resource-group $resourcegroup --query "primaryEndpoints.queue" -o tsv
+$cosmosconnstr = az cosmosdb keys list --name $cosmosdb --resource-group $resourcegroup --type connection-strings --query "connectionStrings[0].connectionString" -o tsv
 
-$queueposturl = "$storageendpoint$storagequeue/messages?$token"
-write-output "This url can be used for posting messages to the queue for the next 5 years:`n $queueposturl"
+az keyvault secret set --vault-name $keyvault --name "CosmosDbConnectionString" --value $cosmosconnstr
 
-Write-Output "saving to environment variable $envvariable"
-# add as environment variable
-[Environment]::SetEnvironmentVariable("$envvariable", $queueposturl, "User")
+# now the function app can read secrets from the key vault by using this url
+#@Microsoft.KeyVault(SecretUri=https://$keyvault.vault.azure.net/secrets/CosmosDbConnectionString)
 
-# ensure that the environment variable is available in the current session
-$env:$envvariable = $queueposturl
+# add the keyvault reference to the function app
+$vaultref = "@Microsoft.KeyVault(SecretUri=https://$keyvault.vault.azure.net/secrets/CosmosDbConnectionString)"
+az functionapp config appsettings set -g $resourcegroup -n $functionappname --settings CosmosDbConnection=$vaultref
 
-Write-Output "Environment variable `n$envvariable `nis now set to `n$queueposturl"
-write-output "It will be available to the git post-commit hook script"
+# # ------------------ THIS IS A TEST COMMITLOGGER QUEUE ----------------
+# # create queue
+# az storage queue create --name $storagequeue --account-name $storageaccount
+
+# # create shared access policy for pushing messages
+# az storage queue policy create --account-name $storageaccount --queue-name $storagequeue --name "addcommits" --permissions "a" --expiry "2028-12-31T23:59:00Z"
+
+# # create a sas token for pushing messages
+# $token = az storage queue generate-sas --account-name $storageaccount --name $storagequeue --policy-name "addcommits" -o tsv 
+
+# Write-Output "SAS token for pushing messages:`n $token"
+
+# $storageendpoint = az storage account show --name $storageaccount --resource-group $resourcegroup --query "primaryEndpoints.queue" -o tsv
+
+# $queueposturl = "$storageendpoint$storagequeue/messages?$token"
+# write-output "This url can be used for posting messages to the queue for the next 5 years:`n $queueposturl"
+
+# Write-Output "saving to environment variable $envvariable"
+# # add as environment variable
+# [Environment]::SetEnvironmentVariable("$envvariable", $queueposturl, "User")
+
+# # ensure that the environment variable is available in the current session
+# $env:$envvariable = $queueposturl
+
+# Write-Output "Environment variable `n$envvariable `nis now set to `n$queueposturl"
+# write-output "It will be available to the git post-commit hook script"
 
